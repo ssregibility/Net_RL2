172e5bedc585fe207c42758859ded77db572d57e
ssregibility <ssregibility@naver.com>
2020-04-30 23:45:55 +0900

    resnext_basis.py updated

--- train.py
+++ train.py
@@ -30,7 +30,7 @@ parser.add_argument('--dataset_path', default="./data", help='dataset path')
 args = parser.parse_args()
 
 if 'CIFAR' in args.dataset:
-    from models.cifar import resnet, resnet_basis, resnext
+    from models.cifar import resnet, resnet_basis, resnext, resnext_basis
 if 'ILSVRC' in args.dataset:
     from models.ilsvrc import resnet, resnet_basis
 
@@ -42,7 +42,7 @@ shared_rank = args.shared_rank
 unique_rank = args.unique_rank
     
 dic_dataset = {'ILSVRC2012':1000, 'CIFAR100':100, 'CIFAR10':10}
-dic_model = {'ResNet50': resnet.ResNet50, 'ResNet34':resnet.ResNet34,'ResNet18':resnet.ResNet18,'ResNet34_Basis':resnet_basis.ResNet34_Basis, 'ResNet34_Unique':resnet_basis.ResNet34_Unique, 'ResNext50':resnext.ResNext50_32x4d, 'ResNext101':resnext.ResNext101_32x8d}
+dic_model = {'ResNet50': resnet.ResNet50, 'ResNet34':resnet.ResNet34,'ResNet18':resnet.ResNet18,'ResNet34_Basis':resnet_basis.ResNet34_Basis, 'ResNet34_Unique':resnet_basis.ResNet34_Unique, 'ResNext50':resnext.ResNext50_32x4d, 'ResNext101':resnext.ResNext101_32x8d, 'ResNext50_Basis':resnext_basis.ResNext50_32x4d_Basis}
 
 if args.dataset not in dic_dataset:
     print("The dataset is currently not supported")
@@ -493,6 +493,86 @@ def train_basis(epoch, include_unique_basis=False):
         loss.backward()
         optimizer.step()
 
+# Training for parameter shared models
+# Use the property of orthogonal matrices;
+# e.g.: AxA.T = I if A is orthogonal 
+def train_basis_resnext(epoch, include_unique_basis=False):
+    if epoch < args.starting_epoch:
+        return
+    
+    print('\nCuda ' + args.visible_device + ' Basis Epoch: %d' % epoch)
+    net.train()
+    
+    for batch_idx, (inputs, targets) in enumerate(trainloader):
+        inputs, targets = inputs.to(device), targets.to(device)
+    
+        optimizer.zero_grad()
+        outputs = net(inputs)
+        
+        # get similarity of basis filters
+        cnt_sim = 0 
+        sim = 0
+        for gid in range(1, 5):  # ResNet has 4 groups
+            layer = getattr(net, "layer"+str(gid))
+            shared_basis = getattr(net,"shared_basis_"+str(gid))
+
+            num_shared_basis = shared_basis.weight.shape[0]
+            num_all_basis = num_shared_basis 
+
+            all_basis =(shared_basis.weight,)
+            if (include_unique_basis == True):  
+                num_unique_basis = layer[1].basis_conv2.weight.shape[0] 
+                num_all_basis += (num_unique_basis * 2 * (len(layer) -1))
+                for i in range(1, len(layer)):
+                    all_basis += (layer[i].basis_conv2.weight,)
+
+            B = torch.cat(all_basis).view(num_all_basis, -1)
+            #print("B size:", B.shape)
+
+            # compute orthogonalities btwn all baisis  
+            D = torch.mm(B, torch.t(B)) 
+
+            # make diagonal zeros
+            D = (D - torch.eye(num_all_basis, num_all_basis, device=device))**2
+            
+            #print("D size:", D.shape)
+         
+            if (include_unique_basis == True):  
+                # orthogonalities btwn shared<->(shared/unique)
+                sim += torch.sum(D[0:num_shared_basis,:])  
+                cnt_sim += num_shared_basis*num_all_basis
+
+                # orthogonalities btwn unique<->unique in the same layer
+                for i in range(1, len(layer)):
+                    for j in range(1):  # conv1 & conv2
+                         idx_base = num_shared_basis   \
+                          + (i-1) * (num_unique_basis) * 2 \
+                          + num_unique_basis * j 
+                         sim += torch.sum(\
+                                 D[idx_base:idx_base + num_unique_basis, \
+                                 idx_base:idx_base+num_unique_basis])
+                         cnt_sim += num_unique_basis ** 2 
+
+            else: # orthogonalities only btwn shared basis
+                sim += torch.sum(D[0:num_shared_basis,0:num_shared_basis])
+                cnt_sim += num_shared_basis**2
+
+        #average similarity
+        avg_sim = sim / cnt_sim
+
+        #acc loss
+        loss = criterion(outputs, targets)
+
+        if (batch_idx == 0):
+            print("accuracy_loss: %.6f" % loss)
+            #print("similarity loss: %.6f" % (-torch.log(1.0-avg_sim)))
+            print("similarity loss: %.6f" % avg_sim)
+
+        #apply similarity loss, multiplied by lambda2
+        #loss = loss - lambda2 * torch.log(1.0 - avg_sim)
+        loss = loss + lambda2 * avg_sim
+        loss.backward()
+        optimizer.step()
         
 #Test for models
 def test(epoch):
@@ -546,7 +626,10 @@ if 'Basis' in args.model:
     optimizer = optim.SGD(net.parameters(), lr=lr, momentum=momentum, weight_decay=weight_decay)
     
     for i in range(150):
-        train_basis(i+1)
+        if 'ResNext' in args.model:
+            train_basis_resnext(i+1)
+        else:
+            train_basis(i+1)
         test(i+1)
     
     #============
@@ -558,7 +641,10 @@ if 'Basis' in args.model:
 
     optimizer = optim.SGD(net.parameters(), lr=lr*0.1, momentum=momentum, weight_decay=weight_decay)
     for i in range(75):
-        train_basis(i+151)
+        if 'ResNext' in args.model:
+            train_basis_resnext(i+151)
+        else:
+            train_basis(i+151)
         test(i+151)
     
     #============
@@ -570,7 +656,10 @@ if 'Basis' in args.model:
 
     optimizer = optim.SGD(net.parameters(), lr=lr*0.01, momentum=momentum, weight_decay=weight_decay)
     for i in range(75):
-        train_basis(i+226)
+        if 'ResNext' in args.model:
+            train_basis_resnext(i+226)
+        else:
+            train_basis(i+226)
         test(i+226)
 
     print("Best_Acc_top1 = %.3f" % best_acc)
