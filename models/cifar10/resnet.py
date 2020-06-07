79d967f5a57a0d88caca4246244a7249f3ea9208
김대연 <ssregibility@gmail.com>
2020-06-07 15:42:48 +0900

    shared basis only resnet for cifar10 added
    Former-commit-id: 9c94b323beff6ec58084ce9500aa7918e72611f6

--- models/cifar10/resnet.py
+++ models/cifar10/resnet.py
@@ -14,6 +14,62 @@ class LambdaLayer(nn.Module):
     def forward(self, x):
         return self.lambd(x)
     
+#BasicBlock for models with shared basis only
+class BasicBlock_Shared(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, shared_basis_1, shared_basis_2, stride=1, option='A'):
+        super(BasicBlock_Shared, self).__init__()
+        
+        self.shared_basis_1 = shared_basis_1
+        self.shared_basis_2 = shared_basis_2
+        
+        self.total_rank_1 = shared_basis_1.weight.shape[0]
+        self.total_rank_2 = shared_basis_2.weight.shape[0]
+        
+        self.basis_bn1 = nn.BatchNorm2d(self.total_rank_1)
+        self.coeff_conv1 = nn.Conv2d(self.total_rank_1, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        self.bn1 = nn.BatchNorm2d(planes)
+        
+        self.basis_bn2 = nn.BatchNorm2d(self.total_rank_2)
+        self.coeff_conv2 = nn.Conv2d(self.total_rank_2, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):           
+        out = self.shared_basis_1(x)
+        out = self.basis_bn1(out)
+        out = self.coeff_conv1(out)
+        
+        out = self.bn1(out)
+        out = F.relu(out, inplace=True)
+        
+        out = self.shared_basis_2(out)
+        out = self.basis_bn2(out)
+        out = self.coeff_conv2(out)
+        
+        out = self.bn2(out)
+
+        out += self.shortcut(x)
+        out = F.relu(out, inplace=True)
+        
+        return out
+    
 #BasicBlock for single basis models
 class BasicBlock_Basis_Single(nn.Module):
     expansion = 1
@@ -130,6 +186,137 @@ class BasicBlock_Basis(nn.Module):
         
         return out
     
+#Original BasicBlock
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1, option='A'):
+        super(BasicBlock, self).__init__()
+        
+        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            if option == 'A':
+                """
+                For CIFAR10 ResNet paper uses option A.
+                """
+                self.shortcut = LambdaLayer(lambda x:
+                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
+            elif option == 'B':
+                self.shortcut = nn.Sequential(
+                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                     nn.BatchNorm2d(self.expansion * planes)
+                )
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out, inplace=True)
+
+        out = self.conv2(out)
+        out = self.bn2(out)
+
+        out += self.shortcut(x)
+        out = F.relu(out, inplace=True)
+        
+        return out
+    
+#ResNet for shared basis only models
+class ResNet_Shared(nn.Module):
+    def __init__(self, block_basis, block_original, num_blocks, shared_rank, num_classes=10):
+        super(ResNet_Shared, self).__init__()
+        self.in_planes = 16
+
+        self.conv1 = nn.Conv2d(3, 16, kernel_size=3, stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(16)
+        
+        self.shared_basis_1_1 = nn.Conv2d(16, shared_rank*1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_1_2 = nn.Conv2d(16, shared_rank*1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block_basis, block_original, 16, num_blocks[0], self.shared_basis_1_1, self.shared_basis_1_2, stride=1)
+        
+        self.shared_basis_2_1 = nn.Conv2d(32, shared_rank*2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_2_2 = nn.Conv2d(32, shared_rank*2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer2 = self._make_layer(block_basis, block_original, 32, num_blocks[1], self.shared_basis_2_1, self.shared_basis_2_2, stride=2)
+        
+        self.shared_basis_3_1 = nn.Conv2d(64, shared_rank*4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_3_2 = nn.Conv2d(64, shared_rank*4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer3 = self._make_layer(block_basis, block_original, 64, num_blocks[2], self.shared_basis_3_1, self.shared_basis_3_2, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(64, num_classes)
+        
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                #initialize every con2d first, then initialize shared basis again later
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+                
+        # Two shared bases are orthogonal-initialized together. 
+        for i in range(1,len(num_blocks)+1):
+            basis_1 = getattr(self, "shared_basis_"+str(i)+"_1")
+            basis_2 = getattr(self, "shared_basis_"+str(i)+"_2")
+            out_chan = basis_1.weight.shape[0]
+            in_chan =  basis_1.weight.shape[1]
+            X = torch.empty(out_chan*2, in_chan, 3, 3)
+            torch.nn.init.orthogonal_(X)
+            basis_1.weight.data = copy.deepcopy(X[:out_chan,:])
+            basis_2.weight.data = copy.deepcopy(X[out_chan:,:])
+
+        # Each share basis is orthogonal-initialized separately
+        # @@ obsolote woochul@2020.05.25
+        '''
+        torch.nn.init.orthogonal_(self.shared_basis_1_1.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_1_2.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_2_1.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_2_2.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_3_1.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_3_2.weight)
+        '''
+
+    def _make_layer(self, block_basis, block_original, planes, blocks, shared_basis_1, shared_basis_2, stride=1):
+        layers = []
+        
+        layers.append(block_original(self.in_planes, planes, stride))
+        
+        self.in_planes = planes * block_original.expansion
+        for _ in range(1, blocks):
+            layers.append(block_basis(self.in_planes, planes, shared_basis_1, shared_basis_2))
+
+        return nn.Sequential(*layers)
+    
+    def _make_layer_original(self, block_original, planes, blocks, stride=1):
+        layers = []
+        
+        layers.append(block_original(self.in_planes, planes, stride))
+        
+        self.in_planes = planes * block_original.expansion
+        for _ in range(1, blocks):
+            layers.append(block_original(self.in_planes, planes, stride))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.bn1(x)
+        x = F.relu(x, inplace=True)
+
+        x = self.layer1(x)
+        x = self.layer2(x)
+        x = self.layer3(x)
+
+        x = self.avgpool(x)
+        x = torch.flatten(x, 1)
+        x = self.fc(x)
+     
+        return x
+    
+#ResNet for single basis only models
 class ResNet_Basis_Single(nn.Module):
     def __init__(self, block_basis, block_original, num_blocks, shared_rank, unique_rank, num_classes=10):
         super(ResNet_Basis_Single, self).__init__()
@@ -201,44 +388,6 @@ class ResNet_Basis_Single(nn.Module):
         x = self.fc(x)
      
         return x
-    
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1, option='A'):
-        super(BasicBlock, self).__init__()
-        
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != planes:
-            if option == 'A':
-                """
-                For CIFAR10 ResNet paper uses option A.
-                """
-                self.shortcut = LambdaLayer(lambda x:
-                                            F.pad(x[:, :, ::2, ::2], (0, 0, 0, 0, planes//4, planes//4), "constant", 0))
-            elif option == 'B':
-                self.shortcut = nn.Sequential(
-                     nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
-                     nn.BatchNorm2d(self.expansion * planes)
-                )
-
-    def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
-        out = F.relu(out, inplace=True)
-
-        out = self.conv2(out)
-        out = self.bn2(out)
-
-        out += self.shortcut(x)
-        out = F.relu(out, inplace=True)
-        
-        return out
     
 class ResNet_Basis(nn.Module):
     def __init__(self, block_basis, block_original, num_blocks, shared_rank, unique_rank, num_classes=10):
@@ -405,3 +554,9 @@ def ResNet56_Single(shared_rank, unique_rank):
 
 def ResNet32_Single(shared_rank, unique_rank):
     return ResNet_Basis_Single(BasicBlock_Basis_Single, BasicBlock, [5, 5, 5], shared_rank, unique_rank)
+
+def ResNet56_Shared(shared_rank):
+    return ResNet_Basis(BasicBlock_Shared, BasicBlock, [9, 9, 9], shared_rank)
+
+def ResNet32_Shared(shared_rank):
+    return ResNet_Basis(BasicBlock_Shared, BasicBlock, [5, 5, 5], shared_rank)
