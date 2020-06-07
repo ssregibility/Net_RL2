ebadb4a2cf3b97d36c53ba7375ecdb9e427c36a0
김대연 <ssregibility@gmail.com>
2020-06-07 15:58:58 +0900

    doulbe basis ilsvrc resnet added

--- models/ilsvrc/resnet.py
+++ models/ilsvrc/resnet.py
@@ -4,11 +4,14 @@ import torch.nn.functional as F
 
 from torch.autograd import Variable
 
-class BasicBlock_Basis(nn.Module):
+import copy
+
+#BasicBlock for single basis models
+class BasicBlock_Single(nn.Module):
     expansion = 1
 
     def __init__(self, in_planes, planes, unique_rank, shared_basis, stride=1):
-        super(BasicBlock_Basis, self).__init__()
+        super(BasicBlock_Single, self).__init__()
         
         self.unique_rank = unique_rank
         self.shared_basis = shared_basis
@@ -52,7 +55,60 @@ class BasicBlock_Basis(nn.Module):
         out = F.relu(out, inplace=True)
         
         return out
+
+#BaiscBlock for double basis models
+class BasicBlock_Basis(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, unique_rank, shared_basis_1, shared_basis_2, stride=1):
+        super(BasicBlock_Basis, self).__init__()
+        
+        self.unique_rank = unique_rank
+        self.shared_basis_1 = shared_basis_1
+        self.shared_basis_2 = shared_basis_2
+        
+        self.total_rank_1 = unique_rank+shared_basis_1.weight.shape[0]
+        self.total_rank_2 = unique_rank+shared_basis_2.weight.shape[0]
+        
+        self.basis_conv1 = nn.Conv2d(in_planes, unique_rank, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.basis_bn1 = nn.BatchNorm2d(self.total_rank_1)
+        self.coeff_conv1 = nn.Conv2d(self.total_rank_1, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        self.bn1 = nn.BatchNorm2d(planes)
+        
+        self.basis_conv2 = nn.Conv2d(planes, unique_rank, kernel_size=3, stride=1, padding=1, bias=False)
+        self.basis_bn2 = nn.BatchNorm2d(self.total_rank_2)
+        self.coeff_conv2 = nn.Conv2d(self.total_rank_2, planes, kernel_size=1, stride=1, padding=0, bias=False)
+        
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion * planes, kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion * planes)
+            )
+
+    def forward(self, x):
+        out = torch.cat((self.basis_conv1(x), self.shared_basis_1(x)),dim=1)
+        out = self.basis_bn1(out)
+        out = self.coeff_conv1(out)
+        
+        out = self.bn1(out)
+        out = F.relu(out, inplace=True)
+        
+        out = torch.cat((self.basis_conv2(out), self.shared_basis_2(out)),dim=1)
+        out = self.basis_bn2(out)
+        out = self.coeff_conv2(out)
+        
+        out = self.bn2(out)
+
+        out += self.shortcut(x)
+        out = F.relu(out, inplace=True)
+        
+        return out
     
+#original BasicBlock
 class BasicBlock(nn.Module):
     expansion = 1
 
@@ -84,9 +140,10 @@ class BasicBlock(nn.Module):
         
         return out
     
-class ResNet_Basis(nn.Module):
+#Proposed resnet (single basis)
+class ResNet_Single(nn.Module):
     def __init__(self, block_basis, block_original, num_blocks, shared_rank, unique_rank, num_classes=1000):
-        super(ResNet_Basis, self).__init__()
+        super(ResNet_Single, self).__init__()
         self.in_planes = 64
 
         self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
@@ -149,7 +206,93 @@ class ResNet_Basis(nn.Module):
         out = self.fc(out)
      
         return out
+    
+#Proposed resnet (double basis)
+class ResNet_Basis(nn.Module):
+    def __init__(self, block_basis, block_original, num_blocks, shared_rank, unique_rank, num_classes=1000):
+        super(ResNet_Basis, self).__init__()
+        self.in_planes = 64
 
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.shared_basis_1_1 = nn.Conv2d(64, shared_rank*1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_1_2 = nn.Conv2d(64, shared_rank*1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer1 = self._make_layer(block_basis, block_original, 64, num_blocks[0], unique_rank*1, self.shared_basis_1_1, self.shared_basis_1_2, stride=1)
+        
+        self.shared_basis_2_1 = nn.Conv2d(128, shared_rank*2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_2_2 = nn.Conv2d(128, shared_rank*2, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer2 = self._make_layer(block_basis, block_original, 128, num_blocks[1], unique_rank*2, self.shared_basis_2_1, self.shared_basis_2_2, stride=2)
+        
+        self.shared_basis_3_1 = nn.Conv2d(256, shared_rank*4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_3_2 = nn.Conv2d(256, shared_rank*4, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer3 = self._make_layer(block_basis, block_original, 256, num_blocks[2], unique_rank*4, self.shared_basis_3_1, self.shared_basis_3_2, stride=2)
+        
+        self.shared_basis_4_1 = nn.Conv2d(512, shared_rank*8, kernel_size=3, stride=1, padding=1, bias=False)
+        self.shared_basis_4_2 = nn.Conv2d(512, shared_rank*8, kernel_size=3, stride=1, padding=1, bias=False)
+        self.layer4 = self._make_layer(block_basis, block_original, 512, num_blocks[3], unique_rank*8, self.shared_basis_4_1, self.shared_basis_4_2, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = nn.Linear(512, num_classes)
+
+        for m in self.modules():
+            if isinstance(m, nn.Conv2d):
+                #initialize every con2d first, then initialize shared basis again later
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
+            elif isinstance(m, (nn.BatchNorm2d, nn.GroupNorm)):
+                nn.init.constant_(m.weight, 1)
+                nn.init.constant_(m.bias, 0)
+                
+        # Two shared bases are orthogonal-initialized together. 
+        for i in range(1,len(num_blocks)+1):
+            basis_1 = getattr(self, "shared_basis_"+str(i)+"_1")
+            basis_2 = getattr(self, "shared_basis_"+str(i)+"_2")
+            out_chan = basis_1.weight.shape[0]
+            in_chan =  basis_1.weight.shape[1]
+            X = torch.empty(out_chan*2, in_chan, 3, 3)
+            torch.nn.init.orthogonal_(X)
+            basis_1.weight.data = copy.deepcopy(X[:out_chan,:])
+            basis_2.weight.data = copy.deepcopy(X[out_chan:,:])
+
+        # Each share basis is orthogonal-initialized separately
+        # @@ obsolote
+        """
+        torch.nn.init.orthogonal_(self.shared_basis_1.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_2.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_3.weight)
+        torch.nn.init.orthogonal_(self.shared_basis_4.weight)
+        """
+
+    def _make_layer(self, block_basis, block_original, planes, blocks, unique_rank, shared_basis_1, shared_basis_2, stride=1):
+        layers = []
+        
+        layers.append(block_original(self.in_planes, planes, stride))
+        
+        self.in_planes = planes * block_original.expansion
+        for _ in range(1, blocks):
+            layers.append(block_basis(self.in_planes, planes, unique_rank, shared_basis_1, shared_basis_2))
+
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = F.relu(out,inplace=True)
+        out = self.maxpool(out)
+
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+     
+        return out
+
+#original ResNet
 class ResNet(nn.Module):
     def __init__(self, block, num_blocks, num_classes=1000):
         super(ResNet, self).__init__()
@@ -208,3 +351,6 @@ def ResNet34():
 
 def ResNet34_Basis(shared_rank, unique_rank):
     return ResNet_Basis(BasicBlock_Basis, BasicBlock, [3, 4, 6, 3], shared_rank, unique_rank)
+
+def ResNet34_Single(shared_rank, unique_rank):
+    return ResNet_Single(BasicBlock_Single, BasicBlock, [3, 4, 6, 3], shared_rank, unique_rank)
